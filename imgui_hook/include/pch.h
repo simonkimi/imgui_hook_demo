c099a123bf5658e17b80687e63a1f38caa04dd79
simonkimi <git@z31.mozmail.com>
2024-01-26 11:18:09 +0800

    添加 imgui-hook 工程

---
+++ imgui_hook/include/pch.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Windows.h"
+#include "detours.h"
+#include <d3d11.h>
+
--- injected_dll/src/main.cpp
+++ injected_dll/src/main.cpp
@@ -13,49 +13,25 @@ std::thread ui_looper;
 std::thread message_looper;
 
 
-//BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-//    if (fdwReason == DLL_PROCESS_ATTACH) {
-//        std::wcout << L"Dll被挂载" << std::endl;
-//        if (!RegisterHotKey(nullptr, HOTKEY_DISPLAY_WINDOWS, MOD_CONTROL | MOD_ALT, VK_F10)) {
-//            std::wcout << std::format(L"注册热键失败, 错误码:{}", GetLastError()) << std::endl;
-//            return FALSE;
-//        }
-//        ui_looper = std::thread(UILopper);
-//        return TRUE;
-//    }
-//
-//    if (fdwReason == DLL_PROCESS_DETACH) {
-//        std::wcout << L"Dll被卸载" << std::endl;
-//        UnregisterHotKey(nullptr, HOTKEY_DISPLAY_WINDOWS);
-//        cancel_token = true;
-//        ui_looper.join();
-//    }
-//
-//    return TRUE;
-//}
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
-    std::wcout << L"Dll被挂载" << std::endl;
-    if (!RegisterHotKey(nullptr, HOTKEY_DISPLAY_WINDOWS, MOD_CONTROL | MOD_ALT, VK_F10)) {
-        std::wcout << std::format(L"注册热键失败, 错误码:{}", GetLastError()) << std::endl;
-        return 1;
-    }
-    ui_looper = std::thread(UILopper);
-
-    MSG msg = {nullptr};
-    while (is_run) {
-        if (GetMessage(&msg, nullptr, 0, 0) > 0) {
-            if (msg.message == WM_HOTKEY) {
-                if (msg.wParam == HOTKEY_DISPLAY_WINDOWS) {
-                    is_display_windows = !is_display_windows;
-                }
-            }
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
+    if (fdwReason == DLL_PROCESS_ATTACH) {
+        std::wcout << L"Dll被挂载" << std::endl;
+        if (!RegisterHotKey(nullptr, HOTKEY_DISPLAY_WINDOWS, MOD_CONTROL | MOD_ALT, VK_F10)) {
+            std::wcout << std::format(L"注册热键失败, 错误码:{}", GetLastError()) << std::endl;
+            return FALSE;
         }
+        ui_looper = std::thread(UILopper);
+        return TRUE;
     }
-    UnregisterHotKey(nullptr, HOTKEY_DISPLAY_WINDOWS);
-    return 0;
+
+    if (fdwReason == DLL_PROCESS_DETACH) {
+        std::wcout << L"Dll被卸载" << std::endl;
+        UnregisterHotKey(nullptr, HOTKEY_DISPLAY_WINDOWS);
+        is_run = false;
+        ui_looper.join();
+    }
+
+    return TRUE;
 }
 
 
